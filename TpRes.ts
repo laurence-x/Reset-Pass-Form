b03c82554d697d41832fb4ff3c4fb5253267e7d6
laurence-x <lpemailx@gmail.com>
2023-01-17 11:07:41 +0200

    Create TpRes.ts

---
+++ TpRes.ts
@@ -0,0 +1,17 @@
+export type checksT = {
+	Pw: React.RefObject<HTMLInputElement>
+	Pw2: React.RefObject<HTMLInputElement>
+	ms: React.RefObject<HTMLParagraphElement>
+	iB: React.RefObject<HTMLInputElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+	hdc: string
+}
+
+export type sendT = {
+	Pw: React.RefObject<HTMLInputElement>
+	Pw2: React.RefObject<HTMLInputElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+	hdc: string
+}
